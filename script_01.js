65f5f20562ea8ee287304d556527308a562d27c7
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-11-24 23:54:14 +0100

    update User Input and corrects first word if its lowercaps and changed it

--- script_01.js
+++ script_01.js
@@ -1,28 +1,15 @@
+let word      = prompt("Füg ein Wort hinzu: ");
+let wordTwo   = prompt("Füg ein weiteres Wort hinzu: ");
+let wordThree = prompt("Füg ein drittes Wort hinzu: ");
+let wordFour  = prompt("Füg ein letztes Wort hinzu: ");
 
-/*** Mini-Challenge  */
-// Satzbau + Punktuation
-
-// Vereinbarung:
-// "S" (sentence) --> "."  Ich bin Max.
-// "Q" (question) --> "?"  Bist du Max?
-// "E" (exclamation) --> "!"  Ich bin!
-
-// Parameter --> funktion getSentence([ ...], "Q"){ ...}
-
-
-/*** Funktion mit Array als Parameter */
-// mögliche Tests:
-// output(getSentence(["Ich","bin","Max"],"S"));
-// output(getSentence(["Bist","du","Max"],"Q"));
-/***************************************************************/
-
-output(getSentence(["I","am","the","Future"]))
-output(getSentence(["Are","you","the","Future"],"s"));
-output(getSentence(["Yes","I","am"],"q"));
+// output(getSentence(["I","am","the","Future"]))
+// output(getSentence(["Are","you","the","Future"],"s"));
+// output(getSentence(["Yes","I","am"],"q"));
+output(userInput());
 
 function getSentence(arr,op)
 {
-   
 	const gap         = " ";
     let str           = "";
     for (let i = 0; i < arr.length; i++) 
@@ -33,7 +20,6 @@ function getSentence(arr,op)
        {
            str += gap;
        }
-
     }
     
     str += marks(op);
@@ -42,14 +28,13 @@ function getSentence(arr,op)
 
 function marks(op)
 {
-    
     if (op == "S" || op == "s")
     {
         return "?";
 
     }else if(op == "Q" || op == "q")
     {
-        return "!";
+        return "!"; 
 
     }else
     {
@@ -57,9 +42,43 @@ function marks(op)
     }
 }
 
-// Modul: Ausgabe in Konsole : Test
-// output("hi"); 
+//Ausgabe in Konsole 
 function output(outputStr)
 {
     console.log(outputStr);
 }
+
+//Function to accept UserInput and Autofinish sentence
+//toDO::: Make a function that checks if a word is a noun 
+// split() method divides a String into and order list of substrings and puts these into an array and returns the array 
+
+function userInput(wordCap)
+{
+    const gapUser = " ";
+    let strUser   = "";
+    var arrUser   = [];
+    arrUser.push(word);
+    arrUser.push(wordTwo);
+    arrUser.push(wordThree);
+    arrUser.push(wordFour);
+
+    if(word.charAt(0).toLowerCase() + word.slice(1))
+    {
+        wordCap = word.charAt(0).toUpperCase() + word.slice(1)
+        arrUser.splice(0,1,wordCap)
+    }
+
+    for (let j = 0; j < arrUser.length; j++)              
+    {
+        strUser += arrUser[j]; 
+
+        if(j < arrUser.length) 
+        {
+            strUser += gapUser;
+        } 
+    }
+    return strUser;
+}
+
+
+
